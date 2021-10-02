f420dbe8ea2465ed2ef47ab44bbb10e23a3c652a
Doyle Fermi <doyle@keyvalue.systems>
2021-10-02 11:58:29 +0000

    Step 1: Node sequelize rest boiler plate (pull request #2)

---
+++ routes/employees.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const validate = require('express-validation');
+const empController = require('../controller/employees');
+const { authorize } = require('../middleware/authorization.middlware');
+const {
+  idParamsSchema,
+  postEmployeeBodySchema,
+  editEmployeeBodySchema,
+  postEmployeeDepartmentBodySchema,
+} = require('../validation/joiRequestValidation');
+
+const router = express.Router();
+
+// GET => /employees
+router.get('/', empController.getAllEmployees);
+
+// GET => /employees/id
+router.get('/:id', validate(idParamsSchema), empController.getEmployee);
+
+// POST => /employees
+router.post('/', validate(postEmployeeBodySchema), authorize, empController.postEmployee);
+
+// PUT => /employees/id
+router.put('/:id', validate(editEmployeeBodySchema), empController.editEmployee);
+
+// DELETE => /employees/id
+router.delete('/:id', empController.deleteEmployee);
+
+// GET => /employees/id/departments
+router.get('/:id/departments', empController.getEmployeeDepartments);
+
+// POST => /employees/id/departments
+router.post('/:id/departments', validate(postEmployeeDepartmentBodySchema), empController.postEmployeeDepartment);
+
+module.exports = router;
